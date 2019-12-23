6d54c2cd3836dac9dffb312468f466485d7feb22
Станислав Шияновский <st-small@rambler.ru>
2019-12-23 19:00:27 +0200

    Initial Commit

---
+++ Dogs breeds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dogs breeds
+//
+//  Created by Stanly Shiyanovskiy on 23.12.2019.
+//  Copyright © 2019 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
